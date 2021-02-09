77740d4ab267db7aef1ab53ca3e3d7c1fea0151f
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2021-02-09 16:37:46 +0100

    Functional app

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
+"""
+Endpoints for generating dynamic images, e.g. plots.
+"""
 import flask
 from flask_caching import Cache
 from flask_cors import CORS
 
-import Covid_portal_vis.Wordcloud.livewordcloud as lwc
+import livewordcloud as lwc
 
 app = flask.Flask(__name__)
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 cors = CORS(app, resources={r'/*': {'origins': '*'}})
 
-@app.route('/img/wordcloud_publications.png')
-@cache.cached(timeout=10800)
-def gen_wordcloud():
-    img = lwc.gen_wordcloud()
+
+@app.route('/wordcloud_<field>.png')
+@cache.cached(timeout=86400)
+def gen_wordcloud_abs(field):
+    if field in ('title', 'abstract'):
+        img = lwc.gen_wordcloud(field)
+    else:
+        return flask.Response(status=404)
     img.seek(0)
     return flask.send_file(img, mimetype='image/png')
